1c2994427e6322e0970e1ae44f32e60182fdf1c8
Amirhossynka <akabiri129@gmail.com>
2023-09-09 23:19:30 +0330

    summarise codes

--- chem.py
+++ chem.py
@@ -13,18 +13,19 @@ class Chem:
 
         molar_mass = 0.0
 
-        compound_dict = parse_formula(compound) # { "H": 2, "O": 1 , "coefficient": 1 }
+        compound_dict = parse_formula(compound)  # { "H": 2, "O": 1 , "coefficient": 1 }
 
-        coefficent = compound_dict.pop("coefficent") # remove coefficients
+        coefficient = compound_dict.pop("coefficient")  # remove coefficients
 
         for key in compound_dict.keys():
-            molar_mass += self.elements_df.loc[self.elements_df["Element"] == key]["MolarMass"].values[0] * compound_dict[key]
+            molar_mass += self.elements_df.loc[self.elements_df["Element"] == key]["MolarMass"].values[0] * \
+                          compound_dict[key]
 
-        return molar_mass * coefficent
+        return molar_mass * coefficient
 
     def __init_elements_df(self):
         """Initializes the data frame"""
-        
+
         try:
             df = pd.read_csv("./data.csv")
             return df
@@ -35,11 +36,12 @@ class Chem:
 
     def fahr_to_c(self, f: Union[int, float]) -> float:
         """
-        # Converts fahrenheit to celsius
+        # Converts Fahrenheit to celsius
         
         chem = Chem()
         chem.fahr_to_c(32.0) # returns 0.0
         """
+        # self.value(f)
         return (f - 32.0) / 1.8
 
     def c_to_fahr(self, c: Union[int, float]) -> float:
@@ -49,9 +51,8 @@ class Chem:
         chem = Chem()
         chem.c_to_fahr(0): # returns 32.0
         """
-
         return (c * 9.0 / 5.0) + 32.0
-   
+
     def get_atomic_number(self, element: str) -> float:
         """
         # Returns the atomic number based on the element symbol or name
@@ -66,14 +67,11 @@ class Chem:
         if element not in self.elements_df[field].values:  # Check for argument
             raise ValueError(f"\n no such {field} -> CHEM.get_atomic_number")
 
-        parameter = self.elements_df[self.elements_df[field] == element]["AtomicNumber"].values[0]
+        atomic_number = self.elements_df[self.elements_df[field] == element]["AtomicNumber"].values[0]
 
-        self.nan(parameter)
-        #if pd.isna(parameter):  # Check for Nan
-         #   raise Value(
-          #      f"\n::No atomic number found for element {element}:: -> CHEM.get_atomic_number()")
+        self.check_nan_value(atomic_number)
 
-        return parameter
+        return atomic_number
 
     def atoms_to_mass(self, quantity: int, chemical: str) -> float:
         """
@@ -93,21 +91,20 @@ class Chem:
         chem.get_element_density("He") # returns ~ 0.0001785
         """
 
-
-
-        if element not in self.elements_df["Element"].values:
-            raise ValueError(f"\n no such element -> chem.get_element_density")
+        self.argument(element)
+        # if element not in self.elements_df["Element"].values:
+        #   raise ValueError(f"\n no such element -> chem.get_element_density")
 
         # Accesses data
-        parameter = self.elements_df.loc[self.elements_df["Element"] == element]["Density"].values[0]
+        density = self.elements_df.loc[self.elements_df["Element"] == element]["Density"].values[0]
 
         # checks for Nan
-        self.nan(parameter)
-            # if pd.isna(density):  # Check for Nan
+        self.check_nan_value(density)
+        # if pd.isna(density):  # Check for Nan
         #    raise ValueError(
-         #       f"\n::No density found for element {element}:: -> chem.get_element_density()")
+        #       f"\n::No density found for element {element}:: -> chem.get_element_density()")
 
-        return parameter
+        return density
 
     def get_element_molarmass(self, element: str) -> float:
         """
@@ -117,12 +114,14 @@ class Chem:
         chem.get_element_molarmass('He') # returns ~ 4.0
         """
 
-        if element not in self.elements_df["Element"].values:
-            raise ValueError("\nno such element -> at chem.get_element_molar_mass")
+        self.argument(element)
+        # if element not in self.elements_df["Element"].values:
+        #   raise ValueError("\nno such element -> at chem.get_element_molar_mass")
 
         return self.elements_df.loc[self.elements_df["Element"] == element]["MolarMass"].values[0]
 
     def get_elementary_elements(self, compound: str, element_g: Union[int, float]) -> float:
+
         """
         # Calculates the number of elementary elements in a compound
         
@@ -132,7 +131,6 @@ class Chem:
 
         molar_mass = self.get_compound_molarmass(compound)
         return (element_g * self.AVOGADRO) / molar_mass
-
 
     def get_compound_molarmass(self, compound: str) -> float:
         """
@@ -148,7 +146,7 @@ class Chem:
         """
 
         return self.__calculate_compound_molarmass(compound)
-   
+
     def get_group(self, element: str) -> str:
         """
         # Get group of element symbol
@@ -156,19 +154,19 @@ class Chem:
         chem = Chem()
         chem.get_group('H') # returns Nonmetal
         """
+        self.argument(element)
+        # if element not in self.elements_df["Element"].values:
+        #   raise ValueError(f"\n no such element -> at chem.get_group")
 
-        if element not in self.elements_df["Element"].values:
-            raise ValueError(f"\n no such element -> at chem.get_group")
+        group_block = self.elements_df.loc[self.elements_df["Element"] == element]["GroupBlock"].values[0]
 
-        parameter = self.elements_df.loc[self.elements_df["Element"] == element]["GroupBlock"].values[0]
+        self.check_nan_value(group_block)
+        # if pd.isna(parameter):  # Check for Nan
+        #   raise ValueError(
+        #      f"\n no block found for element {el} -> at chem.get_group()")
 
-        self.nan(parameter)
-        #if pd.isna(parameter):  # Check for Nan
-         #   raise ValueError(
-          #      f"\n no block found for element {el} -> at chem.get_group()")
+        return group_block
 
-        return parameter
-    
     def get_electron_configuration(self, el: str) -> str:
         """
         # Gets Electron configuration of an element
@@ -177,25 +175,29 @@ class Chem:
         chem.get_electron_configuration("He") # returns "1s2"
         """
 
-        if el not in self.elements_df["Element"].values:
-            raise ValueError(f"\n no such element -> chem.get_electron_configuration()")
+        self.argument(el)
+        # if el not in self.elements_df["Element"].values:
+        #   raise ValueError(f"\n no such element -> chem.get_electron_configuration()")
 
-        parameter = \
-        self.elements_df.loc[self.elements_df["Element"] == el]["ElectronConfiguration"].values[0]
+        electron_configuration = \
+            self.elements_df.loc[self.elements_df["Element"] == el]["ElectronConfiguration"].values[0]
 
-        self.nan(parameter)
-        #if pd.isna(electron_configuration):  # Check for Nan
-         #   raise ValueError(
-          #      f"\n no electron configuration found for element {el} -> CHEM.get_electron_configuration()")
+        self.check_nan_value(electron_configuration)
+        # if pd.isna(electron_configuration):  # Check for Nan
+        #   raise ValueError(
+        #      f"\n no electron configuration found for element {el} -> CHEM.get_electron_configuration()")
 
-        return parameter
+        return electron_configuration
 
-    def nan(self, parameter):
+    def check_nan_value(self, parameter):
         if pd.isna(parameter):
-            raise ValueError(f"\n::NAN value encountered ::")
-        return parameter
+            raise ValueError(f"\n:: there is no record for your element ::")
 
-    def WALTERWHITE(self) -> str:
+    def argument(self, element):
+        if element not in self.elements_df["Element"].values:
+            raise ValueError(f"\n your target element it's not in table, please check your element and try again")
+
+    def WALTERWHITE(self):
         print(
             " ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
             " ⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
@@ -215,12 +217,12 @@ class Chem:
             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⠾⠛⠂⢹⠀⠀⠀⢡⠀⠀⠀⠀⠀⠙⠛⠿⢿\n\n" +
             "Say My Name ")
 
+
 if __name__ == "__main__":
     chem = Chem()
 
-
-    #print(chem.get_group('klsajf')) # returns Nonmetal
-    #print(chem.get_group('He')) # returns Nonmetal
+    # print(chem.get_group('klsajf')) # returns Nonmetal
+    # print(chem.get_group('He')) # returns Nonmetal
     # a = []
     # a.append("H_2O") # ~ 18.0
     # a.append("CaCl_2") # ~ 110.98
@@ -228,5 +230,5 @@ if __name__ == "__main__":
     # for i in a:
     #     print(chem.get_compound_molarmass(i))
     # test values for get_compound_molarmass
-    #print(chem.fahr_to_c(110))
-    print(chem.get_element_density("He"))
+    # print(chem.fahr_to_c(110))
+    # print(chem.get_element_molarmass("He"))
